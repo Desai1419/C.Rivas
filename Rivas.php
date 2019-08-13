5a6ad4e0837f4f43a8929ca28ef15c1ff2ea405a
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-08-13 10:14:50 +0800

    Update Rivas.php

--- Rivas.php
+++ Rivas.php
@@ -8,9 +8,9 @@
   <h2>List of photos</h2>
   <nav>
     <ul>
-      <li><a href="Photo1.html">Photo 1</a></li>
-      <li><a href="Photo2.html">Photo 2</a></li>
-      <li><a href="Photo3.html">Photo 3</a></li>
+      <li><a href="Photo1.php">Photo 1</a></li>
+      <li><a href="Photo2.php">Photo 2</a></li>
+      <li><a href="Photo3.php">Photo 3</a></li>
     </ul>
   </nav>
   </body>

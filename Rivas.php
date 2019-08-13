cfca6fb309e543809266ca7bc5fe982636c163ec
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-08-13 09:59:18 +0800

    Update and rename README.md to Rivas.php

---
+++ Rivas.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Assignment</title>
+  </head>
+<body>
+  <h1>Rivas Photo Gallery</h1>
+  <h2>List of photos</h2>
+  <nav>
+    <ul>
+      <li><a href="Photo1.html">Photo 1</a></li>
+      <li><a href="Photo2.html">Photo 2</a></li>
+      <li><a href="Photo3.html">Photo 3</a></li>
+    </ul>
+  </nav>
+  </body>
+  </html>

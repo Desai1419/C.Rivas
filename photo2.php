2529ded149b049caa690778fd95d59800a58c9d0
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-08-13 10:44:12 +0800

    Update photo2.php

--- photo2.php
+++ photo2.php
@@ -4,7 +4,7 @@
 <head>Photo 2</head>
 <body bgcolor="black">
   <main>
-  <img src="http://studioghiblimovies.com/images/laputaww7.jpg">
+  <img src="http://studioghiblimovies.com/images/laputaww7.jpg" width="800" height="600">
   <a href="index.php">Back to home</a> <a href="photo3.php">Next Photo</a>
     </body>    
 </html>
